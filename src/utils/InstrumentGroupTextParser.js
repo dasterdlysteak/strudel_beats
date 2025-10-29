7cf75ef564c112a3b309c8f54cd4f5b4408a7f5d
Jesse Hamilton-Young <101297717+dasterdlysteak@users.noreply.github.com>
2025-10-29 18:44:53 +1030

    - renamed songeText parser
    realised my regex idea is only going to work for instrument groups and if I want to grab anything else Ill need to come up with another solution

--- src/utils/InstrumentGroupTextParser.js
+++ src/utils/InstrumentGroupTextParser.js
@@ -1,10 +1,10 @@
-export const SongTextParser = (songText) => {
+export const InstrumentGroupTextParser = (songText) => {
     const lines = songText.split('\n');
     const instrumentBlocks = []
     let currentInstrumentBlock = null;
 
     lines.forEach(line => {
-        var title = line.match(/\s/g);// find or figure out regex for word:
+        let title = line.match(/^[A-Za-z0-9_]+:\s*$/);// find or figure out regex for word:
         if (title != null) {
             currentInstrumentBlock = {name: title[0].trim(":"), codeBlock: ""}
 
