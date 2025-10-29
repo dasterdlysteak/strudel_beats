2eea308f8d1c1e990721b5daca7d38b83a407bdd
Jesse Hamilton-Young <101297717+dasterdlysteak@users.noreply.github.com>
2025-10-29 22:06:12 +1030

    - working through the songtext problem
    Im ignoring getting the instrument blocks back into the songText for now and worshipping at the altar of regexes to deal with the fact that instrument blocks can be both a stack() and not a stack() (im going to be very upset if instrument blocks can be other things other than stack or not stack)

--- src/utils/EffectCentre.js
+++ src/utils/EffectCentre.js
@@ -1,7 +1,35 @@
 export const EffectCentre  = {
     changeVolume (InstrumentBlocks, volume) {
-        InstrumentBlocks.forEach(InstrumentBlock => {
-            InstrumentBlock.codeBlock.append(`.gain(${volume})`)
+        InstrumentBlocks.forEach(instrumentBlock => {
+            let body = instrumentBlock.codeBlock
+            let isStack = (/stack\s*\(/.test(body))
+            if (isStack){
+                let stackDepth = 0;
+                let stackEnd = 0;
+                for(let i = 0; i < body.length; i++){
+                    if (body[i] == '('){
+                        stackDepth++;
+                    }else if (body[i] == ')'){
+                        stackDepth--;
+                        if (stackDepth === 0){
+                            stackEnd = i;
+                            break
+                        }
+                    }
+                }
+                const theStack = body.slice(0, stackEnd + 1);
+                let afterStack = body.slice(stackEnd + 1);
+                if (/\.gain\([^]*]\)/.test(afterStack)){
+                    afterStack = afterStack.replace(/\.gain\([^]*\)/, `.gain(${volume})`);
+                }else{
+                    afterStack.append(`.gain(${volume})`);
+                }
+                body = theStack+afterStack;
+            }else{
+            // more logic for searching through an instrument block that is not a stack for .gain()
+            // replacing if exists and appending if not exists
+            }
+            instrumentBlock.codeBlock = body
         })
     }
 }
